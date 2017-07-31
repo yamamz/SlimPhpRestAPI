4d4c685a87778648457ef90cd402d9d85f0c040a
unknown <admin@ITURB.amri.local>
2017-07-31 13:44:43 +0800

    update require in index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-require '/vendor/autoload.php';
+require $_SERVER['DOCUMENT_ROOT'].'/OracleAPI/vendor/autoload.php';
 
 $app = new \Slim\App;
 
-require '/src/routes.php';
+require $_SERVER['DOCUMENT_ROOT'].'/OracleAPI/src/routes.php';
 $app->run();
